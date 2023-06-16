72016f291dc0f0ebea1267db993d783fd1b4c283
rohit <rohitphalke2003@gmail.com>
2023-06-16 17:51:42 +0530

    Worked for deployment on vercel

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,2 @@
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
+pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
